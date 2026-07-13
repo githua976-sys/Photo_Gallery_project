53bbc1ddbade64aadbdee57c9418e6c5eeb44972
githua976sys <githua976@gmail.com>
2026-07-13 18:50:50 +0300

    update setting.py

--- Photo_Gallery_project/settings.py
+++ Photo_Gallery_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^f#r23u%h*ou)_1#7yhm3dkv2a8(+nwn)fdxu^bp&1fwd-$5rg'
+SECRET_KEY = 'config("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
